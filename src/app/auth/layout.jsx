4ada1feda2c96e3a9f7b744a1549d8802a84b9f5
ayhan <ayhandasyuvarlar3@gmail.com>
2024-05-10 18:58:14 +0300

    header auth page hidden , home page visible

--- src/app/auth/layout.jsx
+++ src/app/auth/layout.jsx
@@ -1,3 +1,20 @@
+import { Poppins } from "next/font/google";
+
+import { PrimeReactProvider } from "primereact/api";
+import "primereact/resources/themes/saga-blue/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
+import Providers from "@/redux/provider";
+import Header from "../components/Header";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-poppins",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
 
 export const metadata = {
   title: "Login",
@@ -7,7 +24,18 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
    <>
-    {children}
+<html lang="tr">
+      <PrimeReactProvider>
+        <body
+          className={poppins.className}
+          style={{ maxWidth: 1500, margin: "auto", }}
+        >
+        <Providers>
+          {children}
+        </Providers>
+        </body>
+      </PrimeReactProvider>
+    </html>
    </>
   );
 }
--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,38 +1,49 @@
+'use client'
+import { usePathname } from "next/navigation";
 import React from "react";
 import icons from "../constants/icons";
-import { Button } from "primereact/button";
 import Link from "next/link";
 
 export default function Header() {
-  return (
-    <header className="w-full p-5 grid ">
-      <main className="flex flex-row justify-content-around gap-4 w-full">
-        <h1 className="text-2xl flex w-30rem">
-          <Link href="/" className="text-blue-900">
-            E-commerce
-          </Link>
-        </h1>
-        <nav className="flex w-30rem flex flex-row justify-content-end align-items-center">
-          <ul className="flex flex-row align-items-center gap-3">
-            <li className="">
-            <Link
-                href="/basket"
-                className="p-2 bg-transparent border-bluegray-200 border-round flex justify-content-center align-items-center border-1"
-              >
-                <icons.TbBasketCheck size={20} color="black" />
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/auth/login"
-                className="p-2 bg-transparent border-bluegray-200 border-round flex justify-content-center align-items-center border-1"
-              >
-                <icons.AiOutlineLogin size={20} color="black" />
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </main>
-    </header>
-  );
+  const pathname = usePathname();
+
+  // "/auth/login" veya "/auth/register" için aynı header yapısını kullanın
+  const isAuthPage = pathname.startsWith("/auth");
+
+  if (isAuthPage) {
+    return null; // Eğer auth sayfasındaysak hiçbir şey gösterme
+  } else {
+    return (
+      <header className="w-full p-5 grid ">
+        <main className="flex flex-row justify-content-around gap-4 w-full">
+          <h1 className="text-2xl flex w-30rem">
+            <Link href="/" className="text-blue-900">
+              E-commerce
+            </Link>
+          </h1>
+          <nav className="flex w-30rem flex flex-row justify-content-end align-items-center">
+            <ul className="flex flex-row align-items-center gap-3">
+              <li className="">
+                <Link
+                  href="/basket"
+                  className="p-2 bg-transparent border-bluegray-200 border-round flex justify-content-center align-items-center border-1"
+                >
+                  <icons.TbBasketCheck size={20} color="black" />
+                </Link>
+              </li>
+              {/* Auth sayfaları için login ve register aynı şekilde görüntülensin */}
+              <li>
+                <Link
+                  href="/auth/login"
+                  className="p-2 bg-transparent border-bluegray-200 border-round flex justify-content-center align-items-center border-1"
+                >
+                  <icons.AiOutlineLogin size={20} color="black" />
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </main>
+      </header>
+    );
+  }
 }
