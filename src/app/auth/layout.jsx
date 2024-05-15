4533f81215f44e39f0d6e45bc20191ab745e42e4
ayhan <ayhandasyuvarlar3@gmail.com>
2024-05-15 12:56:07 +0300

    title changed

--- src/app/auth/layout.jsx
+++ src/app/auth/layout.jsx
@@ -2,14 +2,12 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-export default function layout({ children }) {
+export default function Layout({ children }) {
   const pathname = usePathname();
   const isLoginPage = pathname.startsWith("/auth/login");
 
   return (
-    <main
-      className=" flex flex-row h-screen align-items-center justify-content-center  p-0  m-auto"
-    >
+    <main className=" flex flex-row h-screen align-items-center justify-content-center  p-0  m-auto">
       <div
         className="flex  flex-column border-1  w-6 border-gray-300 border-round-xl overflow-hidden"
         style={{ height: "80vh" }}
@@ -18,7 +16,7 @@ export default function layout({ children }) {
           <p className="text-4xl font-bold">Welcome Back 👋🏻</p>
           {isLoginPage ? (
             <p className="text-lg text-black-alpha-50 ">
-              Don't have a account{" "}
+              Don't have a account
               <Link className="underline" href={"/auth/register"}>
                 Sign up
               </Link>
